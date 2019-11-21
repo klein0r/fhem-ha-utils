2de3922a860dd49c0b60ef5e0ca66838f0451527
Matthias Kleine <matthias@klein0r.de>
2019-11-21 11:37:58 +0100

    Prepare Update

--- prepare_update.sh
+++ prepare_update.sh
@@ -4,7 +4,7 @@
 
 rm controls_ha_utils.txt
 
-find ./www ./FHEM -type f \( ! -iname ".*" \) -print0 | while IFS= read -r -d '' f;
+find ./FHEM -type f \( ! -iname ".*" \) -print0 | while IFS= read -r -d '' f;
 do
     out="UPD "$(stat -f "%Sm" -t "%Y-%m-%d_%T" $f)" "$(stat -f%z $f)" ${f}"
     echo ${out//.\//} >> controls_ha_utils.txt
